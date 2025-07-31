35111d90d0c0d5edb8bdd6d8ded1fc145b771290
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-08-01 00:29:54 +0530

    wait

--- src/components/pages/getData.jsx
+++ src/components/pages/getData.jsx
@@ -23,8 +23,25 @@ function GetData() {
         
 
       </div>
-      <div className="min-w-[280px] h-full border-2 mx-2 lg:m-0 flex flex-col">
+      <div className="min-w-[280px] lg:w-[250px] h-full border-2 mx-2 lg:m-0 flex flex-col">
           <h1 className="bg-amber-700 border-b-2 text-center">INPROGRESS</h1>
+          <div className="bg-yellow-700 w-full flex-grow overflow-y-auto  flex flex-col gap-4 p-2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum amet eaque culpa dolor debitis cupiditate voluptatum tempore distinctio adipisci. Nulla inventore hic optio suscipit reiciendis beatae laboriosam ducimus magnam molestiae!
+            Incidunt, voluptatem hic culpa excepturi impedit perferendis laudantium sapiente. Ullam aut illum animi molestiae quod nesciunt mollitia tempore cumque, perspiciatis ipsa, neque odio? Veniam, quis quia. Sapiente harum necessitatibus cum.
+            Veritatis vel in dolor voluptatibus nesciunt saepe corrupti, nulla fugit sed labore quae itaque, voluptates consectetur dignissimos non perferendis asperiores! Quae, quos veritatis vel eveniet aliquid unde sequi enim laudantium.
+            Rerum corporis dolorum, tempora perferendis vitae, ea earum eum officiis nam illo, pariatur harum reprehenderit officia autem blanditiis nihil unde dignissimos ipsa reiciendis itaque. Suscipit magni deleniti ipsum ex beatae!
+            Adipisci, necessitatibus earum, suscipit omnis vitae qui corrupti quasi labore repudiandae, perspiciatis quam maxime doloremque aspernatur? Maxime reiciendis earum magni perferendis? Accusamus architecto numquam dolore iusto ducimus labore, asperiores quia.
+            Voluptatem quidem tempora adipisci officia doloremque. Impedit a dolore quae possimus! Assumenda necessitatibus, eaque cupiditate harum repudiandae ipsa. Ipsam, praesentium saepe neque ducimus perferendis hic numquam dolorum! Rerum, labore excepturi?
+            Quas nam recusandae tenetur eos laborum debitis minima veritatis eligendi enim quaerat, exercitationem nostrum nisi, beatae eum natus possimus omnis sit soluta perspiciatis voluptatem optio iste obcaecati sed odit? Cum.
+            Quaerat, sit eos est quidem sint aliquid? Aut sequi culpa accusantium exercitationem dicta facilis sit assumenda ullam, obcaecati dignissimos qui sed ea inventore, officiis ex sint? Adipisci hic sequi molestiae.
+            Dicta doloremque odio possimus qui quia illo sint incidunt maxime laborum veritatis, quasi, aperiam quod, molestias excepturi nam facere blanditiis assumenda! Quidem delectus laboriosam itaque ipsam nihil nostrum, molestias dolores.
+            Minima aut quibusdam cumque. Corrupti cupiditate recusandae neque ducimus ea officia voluptate quia accusantium laudantium quidem ipsa nobis fugiat, praesentium, sapiente autem. Id odit porro magni quia saepe ex accusamus?
+            Debitis, deleniti dolores eum molestias ex, quod officiis quas tempore possimus maiores id eius voluptas, rem ipsam amet iste similique voluptate est recusandae aut placeat? Ut repudiandae quod nihil non.
+            Vero adipisci possimus accusantium voluptatum alias! Fugit impedit iste, minus numquam dolorum voluptatem. Perspiciatis incidunt placeat illum, ullam nulla exercitationem vero quas vel saepe aliquam voluptatum ad, voluptas impedit id!
+            Obcaecati minima exercitationem ipsam fugit dolor voluptatibus aspernatur, pariatur excepturi perspiciatis assumenda, quis incidunt, ad ullam? Quibusdam perferendis corporis doloremque nostrum voluptas rem, earum quos suscipit neque quam quaerat exercitationem?
+            In doloribus numquam at fuga expedita unde pariatur voluptatibus dolor? Accusamus earum quam alias in corrupti explicabo aliquam dolore perspiciatis, est quis minus laborum blanditiis neque nihil impedit, ex nam.
+            Porro delectus corporis architecto dolores fuga saepe incidunt consequatur repellendus recusandae ducimus minima ullam dicta culpa molestias ab unde autem quae cum impedit animi, eius deleniti fugit. Hic, beatae itaque?</p>
+          </div>
         </div>
         <div className="min-w-[280px] h-full border-2 mx-2 lg:m-0 flex flex-col">
           <h1 className="bg-amber-700 border-b-2 text-center">DONE</h1>
